2089c8a8501326a2084bcb5a205ad4836ef98327
EOJJIN <uhrealll@gmail.com>
2022-06-27 21:43:50 +0900

    Spring Data JPA 적용

---
+++ src/main/java/com/study/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.study.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //모든 필드의 Getter 메소드 자동생성
+@NoArgsConstructor //기본 생성자 자동 추가
+@Entity //테이블과 연결할 클래스
+public class Posts {
+
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false) //선언하지 않아도 해당 클래스의 필드는 모두 칼럼이지만, 변경 옵션이 있을 시 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
